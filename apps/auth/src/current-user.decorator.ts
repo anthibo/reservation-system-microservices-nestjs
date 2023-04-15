c8a2ee5d89d2ddfeefed083fe38c3c0a3a75add7
procrew <anthibosama@gmail.com>
2023-04-15 23:36:28 +0200

    feat: implement local strategy

---
+++ apps/auth/src/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { UserDocument } from './users/models/user.schema';
+
+const getCurrentUserByContext = (context: ExecutionContext) => {
+  const request = context.switchToHttp().getRequest();
+  return request.user as UserDocument;
+};
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) =>
+    getCurrentUserByContext(context),
+);
