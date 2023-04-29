6afd84c7e535a6d28302446cb34769228f5bdb1d
procrew <anthibosama@gmail.com>
2023-04-29 06:18:38 +0300

    fix: get proto file path based on environment

--- libs/common/src/proto/payments/index.ts
+++ libs/common/src/proto/payments/index.ts
@@ -1,5 +1,13 @@
+import * as path from 'path';
+
 export * from './payments.service.interface';
-export const PAYMENTS_PROTO_PATH =
-  'libs/common/src/proto/payments/payments.proto';
+
+let payment_proto_path = 'libs/common/src/proto/payments/payments.proto';
+
+if (process.env.NODE_ENV === 'production') {
+  payment_proto_path = path.resolve(__dirname, 'payments.proto');
+}
+
+export const PAYMENTS_PROTO_PATH = payment_proto_path;
 export const PAYMENTS_PROTO_SERVICE = 'PaymentsService';
 export const PAYMENTS_PROTO_PACKAGE = 'payments';
